bc929dca06193428e7a0c3affa1283bd270df21f
ameanasad <amean.asad1999@gmail.com>
2023-08-08 09:05:14 -0400

    feat: add abi writer on deploys

--- contract-utils/src/main.rs
+++ contract-utils/src/main.rs
@@ -1,13 +1,16 @@
+use contract_bindings::measure::MEASURE_ABI;
 use contract_utils::measure::{deploy_contract, DeployMethod};
 use ethers::providers::Middleware;
+use ethers::utils::__serde_json::ser;
 use fevm_utils::{get_ledger_signing_provider, get_provider, get_wallet_signing_provider};
-use std::fs::read_to_string;
+use std::fs::{read_to_string, write};
 use std::sync::Arc;
 use std::{path::PathBuf, str::FromStr};
 
 const DEPLOY_METHOD: DeployMethod = DeployMethod::Mnemonic;
 const RPC_URL: &str = "https://api.calibration.node.glif.io/rpc/v1";
 const SECRETS_PATH: &str = "../secrets/mnemonic";
+const ABI_WRITE_PATH: &str = "../MeasureAbi.json";
 
 const RETRIES: usize = 15;
 
@@ -15,6 +18,9 @@ const RETRIES: usize = 15;
 async fn main() {
     let provider = get_provider(&RPC_URL).unwrap();
     let chain_id = provider.get_chainid().await.unwrap();
+    let string_abi = ser::to_string(&MEASURE_ABI.clone()).unwrap();
+    write(&ABI_WRITE_PATH, string_abi).unwrap();
+
     match DEPLOY_METHOD {
         DeployMethod::Ledger => {
             let ledger_client = get_ledger_signing_provider(provider.clone(), chain_id.as_u64())
